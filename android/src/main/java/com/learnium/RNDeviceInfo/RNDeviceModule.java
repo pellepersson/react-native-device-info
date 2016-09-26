347a5bd40b1ce4ce30fbcfabf5f002555d849031
Pelle P <hej@pellepersson.se>
2016-09-26 17:45:27 +0200

    removed instanceID from android version and remove dependency to android-gms

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -6,8 +6,6 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings.Secure;
 
-import com.google.android.gms.iid.InstanceID;
-
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
@@ -80,7 +78,6 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
       e.printStackTrace();
     }
 
-    constants.put("instanceId", InstanceID.getInstance(this.reactContext).getId());
     constants.put("deviceName", deviceName);
     constants.put("systemName", "Android");
     constants.put("systemVersion", Build.VERSION.RELEASE);
